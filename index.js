3459ed999138afef9fad14a6a9c13096993225e3
Kelvin <vokemalosh32@gmail.com>
2022-06-19 16:18:40 +0300

    commit tree 1af2dc82885c1991b32570008fd3f8dc4f1c0e69

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  time complexity of this algorithm is O(n)
+  space complexity of this algorithm is O(n)
+
 */
 
 /* 
-  Add your pseudocode here
+  create an object to keep track of all the numbers we've seen
+iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  check if any of the keys in our object is the complement to the current number
+    if so, return true
+  save the current number as the key on our object so we can check it later against other numbers
+if we reach the end of the array, return false
 */
 
 /*
   Add written explanation of your solution here
+  As we iterate through the numbers in the array,
+  we can create a new object as part of our algorithm 
+  to keep track of all the numbers we've already seen 
+  as keys on the object, so we can quickly check if the
+   object has a key that we're looking for. Then, on the
+    next iteration, we can see if one of the numbers we've 
+    added as a key to our object is a complement to the number 
+    we're iterating over (if it adds up to the target number).
 */
 
 // You can run `node index.js` to view these console logs
